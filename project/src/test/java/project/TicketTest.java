a6e0cdafb0bd79d3ccf8a5431cdaee15ceec631f
Nakata <Nakata@DESKTOP-8G0IB98>
2021-02-27 00:00:31 +0200

    project

--- project/src/test/java/project/TicketTest.java
+++ project/src/test/java/project/TicketTest.java
@@ -1,10 +1,17 @@
 package project;
 
+import static org.junit.Assert.assertEquals;
+
 public class TicketTest 
 {
 	public void test1() {
 		String answer = TicketTest.addTicket("2$", "seat 5");
 		assertEquals(answer, "no");
+	}
+
+	private static String addTicket(String string, String string2) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
